c76a18fe0dbeb9dcf2f0f1256a0315ed7617f3e0
ionescuale <ionescu.ale@gmail.com>
2023-03-30 18:11:40 +0300

    first commit

--- Bucatarie/src/reteta.py
+++ Bucatarie/src/reteta.py
@@ -5,6 +5,7 @@ class Reteta:
         self.nume = nume
         self.reteta = {}
 
+
     def reteta(self, numeReteta):
         filename = 'data/' + numeReteta + '.pickle'
         with open(filename, 'rb') as f:
